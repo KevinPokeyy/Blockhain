4ebdb554cf00be7eed190d8b9bc3a411ec7aecf7
TheTadej123 <73063055+TheTadej123@users.noreply.github.com>
2021-05-29 18:54:55 +0200

    fixed

--- Jezik/Naše/creators/Action.kt
+++ Jezik/Naše/creators/Action.kt
@@ -1,6 +1,6 @@
 class Action() {
     val BuyItems: MutableList<BuyItem> = mutableListOf<BuyItem>();
     val SellItems: MutableList<SellItem> = mutableListOf<SellItem>();
-    val TransferMoney: MutableList<Variable> = mutableListOf<Variable>();
-    val TransferItem: MutableList<Variable> = mutableListOf<Variable>();
+    val TransferMoney: MutableList<TransferMoney> = mutableListOf<TransferMoney>();
+    val TransferItem: MutableList<TransferItem> = mutableListOf<TransferItem>();
 }
